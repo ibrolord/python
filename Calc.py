34394b9b4edb3709db3c4d975c3d6d6f7a1cdd99
ibrolord <princebolajibreeze@gmail.com>
2019-09-25 12:30:09 -0400

    Some quick Python code

--- Calc.py
+++ Calc.py
@@ -3,49 +3,61 @@
 #Exception will ensure that the StdOut is as Neat as Possible
 
 try:
-  #Function to Add 2 Numbers
-  def add(x, y):
-      return x + y
+  while True:
+    #Function to Add 2 Numbers
+    def add(x, y):
+        return x + y
 
-  #Function to Subtract 2 Numbers
-  def sub(x, y):
-      return x - y 
+    #Function to Subtract 2 Numbers
+    def sub(x, y):
+        return x - y 
 
-  #Function to Multiply 2 Numbers
-  def mult(x, y):
-      return x * y
+    #Function to Multiply 2 Numbers
+    def mult(x, y):
+        return x * y
 
-  #Function to Divide 2 Numbers
-  def div(x, y):
-      return x / y
+    #Function to Divide 2 Numbers
+    def div(x, y):
+        return x / y
 
-  #Output Texts
-  print("Select the Operations you want to Perform")
-  print("Enter 'add' to add 2 numbers \n Enter 'sub' to Subtract 2 Numbers \n Enter 'div' to Divide 2 Numbers \n ")
+    #Output Texts
+    print("\nSelect the Operations you want to Perform")
+    print("Enter 'add' to add 2 numbers \n Enter 'sub' to Subtract 2 Numbers \n Enter 'div' to Divide 2 Numbers \n Enter 'quit' to quit")
 
-  #Take User Inputs
-  op = input('Enter Choice(add, sub, mult, div): ')
-  print("You want to" , str(op))
+    #Take User Inputs
+    op = input('Enter Choice(add, sub, mult, div, quit): ')
+    print("You want to" , str(op))
+    
+    #Make sure user enters something 
+    while op != 'add' and op != 'sub' and op != 'mult' and op != 'div' and op != 'quit':
+       print(f"You must have entered a Bad Operator {op}")
+       op = input('\nEnter Choice(add, sub, mult, div, quit): ')
+       continue
 
-  #Assign numbers to Variables; Wrap as integer - No Floats
-  num1 = int(input("Enter your First Number: "))
-  num2 = int(input("Enter your Second Number: "))
+    #Allow users Quit Early
+    if op == 'quit':
+        print("GoodBye")
+        break
 
-  #Begin Logic
-  if op == 'add':
-      print(num1,"+",num2,"=", add(num1,num2))
+    #Assign numbers to Variables; Wrap as integer - No Floats
+    num1 = int(input("Enter your First Number: "))
+    num2 = int(input("Enter your Second Number: "))
 
-  elif op == 'sub':
-      print(num1,"-",num2,"=", sub(num1,num2))
+    #Begin Logic
+    if op == 'add':
+        print(num1,"+",num2,"=", add(num1,num2))
 
-  elif op == 'mult':
-      print(num1,"-",num2,"=", mult(num1,num2))
+    elif op == 'sub':
+        print(num1,"-",num2,"=", sub(num1,num2))
 
-  elif op == 'div':
-      print(num1,"-",num2,"=", div(num1,num2))
+    elif op == 'mult':
+        print(num1,"-",num2,"=", mult(num1,num2))
 
-  else:
-      print("I don't know what you typed")
+    elif op == 'div':
+        print(num1,"-",num2,"=", div(num1,num2))
+
+    else:
+        print("I don't know what you typed, check your Operator")
 
 except Exception as e:
     print(f"You must have done something wrong, Error is: '{e}'" )
